0dc2d701dd8127331627758a71632ca8e42f317b
aliilhan <75568951+aliilhangis@users.noreply.github.com>
2026-04-13 16:15:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
-from scraper import scrape_product
 import asyncio
+from fastapi import FastAPI, HTTPException
+from scraper import scrape_product
 
 app = FastAPI()
 
 @app.get("/scrape")
 async def scrape(url: str):
-    data = await scrape_product(url)
-    return data
+    try:
+        data = await asyncio.wait_for(scrape_product(url), timeout=120)
+        return data
+    except asyncio.TimeoutError:
+        raise HTTPException(status_code=504, detail="Scraping timeout - 120 saniye aşıldı")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
